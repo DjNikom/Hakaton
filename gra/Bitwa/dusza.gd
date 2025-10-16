dcbd2ec8a8ac55f0c68f63a2e6d7876616118ac2
Mint <mici5dabestpl@gmail.com>
2025-10-16 11:12:00 +0200

    dodano system zmieniania koloru duszy

--- gra/Bitwa/dusza.gd
+++ gra/Bitwa/dusza.gd
@@ -1,12 +1,24 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 200.0
+var mode = 0 #0 - red, 1 - blue
 
 func _physics_process(_delta):
-	var dir_x = Input.get_axis("player_left", "player_right")
-	var dir_y = Input.get_axis("player_up", "player_down")
-	
-	velocity.x = dir_x * SPEED
-	velocity.y = dir_y * SPEED
+	if mode == 0:
+		self.modulate = Color(1, 0, 0)
+		var dir_x = Input.get_axis("player_left", "player_right")
+		var dir_y = Input.get_axis("player_up", "player_down")
+		
+		velocity.x = dir_x * SPEED
+		velocity.y = dir_y * SPEED
 
-	move_and_slide()
+		move_and_slide()
+	if mode == 1:
+		self.modulate = Color(0, 0, 1)
+		var dir_x = Input.get_axis("player_left", "player_right")
+		var dir_y = Input.get_axis("player_up", "player_down")
+		
+		velocity.x = dir_x * SPEED
+		velocity.y = dir_y * SPEED
+
+		move_and_slide()
